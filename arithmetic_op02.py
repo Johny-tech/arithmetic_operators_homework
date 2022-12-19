c8830acc8510eb61a949c40e661e5948212fb69b
Johny-tech <limonsoft.rustam05@gmail.com>
2022-12-19 16:47:43 -0500

    notes

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,3 +1,9 @@
+#Create a varaible 'number' and assign it the value of 43.
+
+#Create a variable 'answer' and assign it the sum of the number's digits.
+
+#Print the vallue of the 'answer'.
+
 number = 43 
 d1 = number//10
 d2 = number%10
--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,3 +1,9 @@
+#Create a variable called 'number' and assign it the two-digit number
+
+#Find the reverse of the number and assign it to a variable called 'answer'.
+
+#Print the value of the 'answer'
+
 number = 43 
 d1 = number//10
 d2 = number%10
