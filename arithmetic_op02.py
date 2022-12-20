c0772a8f3fd346fa54ab812f89b71b72fb36ec19
Johny-tech <limonsoft.rustam05@gmail.com>
2022-12-20 11:33:29 -0500

    changed

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,10 +1,10 @@
 #Create a varaible 'number' and assign it the value of 43.
-
-#Create a variable 'answer' and assign it the sum of the number's digits.
-
-#Print the vallue of the 'answer'.
-
 number = 43 
 d1 = number//10
 d2 = number%10
-print(d1+d2)
+#Create a variable 'answer' and assign it the sum of the number's digits.
+answer = d1+d2
+#Print the vallue of the 'answer'.
+
+
+print(answer)
